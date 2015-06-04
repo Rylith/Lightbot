0329fe077f460a92fce73009c5d057c0170a4e5a
Rylith <ogaoni@gmail.com>
2015-06-04 15:19:08 +0200

    Modif JRE et test affichage sprite

--- src/lightbot/jsfmltesto.java
+++ src/lightbot/jsfmltesto.java
@@ -1,7 +1,15 @@
 package lightbot;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import org.jsfml.graphics.Color;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
+import org.jsfml.system.Clock;
+import org.jsfml.system.Vector2f;
+import org.jsfml.system.Vector2i;
 import org.jsfml.window.VideoMode;
 import org.jsfml.window.event.Event;
 
@@ -10,15 +18,36 @@ public class jsfmltesto {
     public static void main(String[] args) {
  
         RenderWindow fenetre = new RenderWindow();
+        Texture textureTile = new Texture();
  
-        fenetre.create(new VideoMode(640, 480), "Ma premiere fenetre JSFML");
+        fenetre.create(new VideoMode(640, 480), "Test d'affichage de map");
  
         // Boucle principale qui s’exécute tant que la fenêtre est ouverte
         while (fenetre.isOpen()) {
  
             fenetre.clear(Color.BLACK); // On remplit la fenêtre avec la couleur
                                         // noire
- 
+            try {
+                //Try to load the texture from file "jsfml.png"
+            	textureTile.loadFromFile(Paths.get("tile.png"));
+
+                //Texture was loaded successfully - retrieve and print size
+                Vector2i size = textureTile.getSize();
+            } catch(IOException ex) {
+                //Ouch! something went wrong
+                ex.printStackTrace();
+            }
+          //Create a sprite and make it use the logo texture
+            Sprite tile1 = new Sprite(textureTile);
+            Sprite tile2 = new Sprite(textureTile);
+
+            //Set its origin to its center and put it at the center of the screen
+            tile1.setOrigin(Vector2f.div(new Vector2f(textureTile.getSize()), 2));
+            tile1.setPosition(320, 240);
+            tile2.setOrigin(Vector2f.div(new Vector2f(textureTile.getSize()), 2));
+            tile2.setPosition(320+130/2,235+75/2);
+            fenetre.draw(tile1);
+            fenetre.draw(tile2);
             fenetre.display();// On affiche notre fenetre et ce qu'on doit
                                 // dessiner dessus (la couleur noire )
  
