0d02c132f77b607031484d2eb9daf7177385070d
Rylith <ogaoni@gmail.com>
2015-06-05 15:02:36 +0200

    Ajout classe Button

---
+++ lightbot/src/lightbot/Button.java
@@ -0,0 +1,36 @@
+package lightbot;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
+import org.jsfml.system.Vector2f;
+import org.jsfml.system.Vector2i;
+
+public class Button {
+	
+	private Sprite button_sprite;
+	
+	public Button(String texture_file,float x, float y) {
+		Texture textureButton = new Texture();
+		try {
+            //Try to load the texture from file "jsfml.png"
+        	textureButton.loadFromFile(Paths.get(texture_file));
+        	
+        } catch(IOException ex) {
+            //Ouch! something went wrong
+            ex.printStackTrace();
+        }
+		this.button_sprite = new Sprite(textureButton);
+		this.button_sprite.setPosition(x, y);
+	}
+	
+	public boolean clicked(Vector2i pos){
+		return this.button_sprite.getGlobalBounds().contains(pos.x, pos.y);
+	}
+	
+	public Sprite getSprite(){
+		return this.button_sprite;
+	}
+}
