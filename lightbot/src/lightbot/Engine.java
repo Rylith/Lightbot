62534ec7ba0a4de15660f2f0daf52118c6cf8f3d
yacinend <yacinenana@gmail.com>
2015-06-09 09:51:46 +0200

    modifications de engine, turnLeft, turnRight, Order, ajout de JUMP

--- lightbot/src/lightbot/Engine.java
+++ lightbot/src/lightbot/Engine.java
@@ -1,45 +1,129 @@
 package lightbot;
+import org.jsfml.system.Vector2f;
 
-import lightbot.Map;
-import lightbot.Personnage;
 
 public class Engine {
 	
 	//private Personnage personne;
 	
-	private Map monde;
+	private static final int encadrement;
 	
-	public Engine(Map m){
-			
-		this.setMonde(m);
-	}
-
-	
-	
-	public boolean ExecMove(Personnage p){
+	/** t
+	 * 
+	 * @param clone: le clone deplace a la case de destination, hauteur inchangée
+	 * @return true si pas d'obstacles, false sinon
+	 * @obstacle1: si les cases de depart et d'arrivee ont des hauteurs differentes
+	 */
+	private boolean isAbleToMove(Character clone){
 		
-		Personnage clone;
+		//il avance pas si la hauteur de la case dest est differente de celle de depart
+		
+		if(clone.getHeight() != getCurrentCase(clone).getHeight())
+			
+			return true;
+		
+		else 
+			return false;	
+		
+	}
+	/** t
+	 * 
+	 * @param clone: le clone deplace a la case de destination, hauteur inchangée
+	 * @return true si pas d'obstacles au jump, false sinon
+	 * @obstacle1: si les cases de depart et d'arrivee sont de meme hauteur
+	 * @obstacle2 : 
+	 */
+	
+	private boolean isAbleToJump(Character clone){
+		
+		
+	}
+	
+	private boolean encadrementOK(float x, float y, Character p){
+		
+		return  (
+				
+				p.getPosition().x >= x- encadrement &&
+				p.getPosition().x <= x+ encadrement &&
+				p.getPosition().y >= y- encadrement &&
+				p.getPosition().y <= y+ encadrement );
+			
+		
+	}
+	/**
+	 * 
+	 * @param p: le personnage sur la case
+	 * @return la case ou est le personnage
+	 */
+	private Case getCurrentCase(Character p) {
+		// TODO Auto-generated method stub
+		float ideal_x, ideal_y;
+		
+		//for i allant de 1 a la fin de toutes les cases
+		
+			if(t[i].isContain(p.getPosition()))
+				
+				if(encadrementOK(ideal_x, ideal_y,p))
+					return t[i];
+				else
+					return null;
+			else
+				return null;
+		
+	}
+	
+	/**
+	 * fait avancer le personnage d'une case en fonction de la direction
+	 * @param p: le personnage a faire avancer
+	 * @return un booleen : true si le move s'est bien passe, false sinon
+	 */
+
+	public boolean ExecMove(Character p){
+		
+		Character clone;
+		Vector2f temp;
+		
 		clone=p;
 		float a,b,c,d;
-		a = p.getPosition().x - getCurrentCase(p).sprite.getSpriteSize.x;
 		
-		switch(clone.getDirection()){
+		a = p.getPosition().x - getCurrentCase(p).getSprite().getTexture().getSize().x; 
+		b =  p.getPosition().x + getCurrentCase(p).getSprite().getTexture().getSize().x;
+		c =  p.getPosition().y - getCurrentCase(p).getSprite().getTexture().getSize().y;
+		d = p.getPosition().y + getCurrentCase(p).getSprite().getTexture().getSize().y;
 		
-		case Up:  clone.setPosition()
+		switch(clone.getOrientation()){
+		
+		case Up: temp = new Vector2f(a,c); clone.setPosition(temp); break;
+		case Down: temp = new Vector2f(b,d); clone.setPosition(temp); break;
+		case Right: temp = new Vector2f(b,c); clone.setPosition(temp); break;
+		case Left: temp = new Vector2f(a,c); clone.setPosition(temp); break;
+		default: 
+		
 		}
 		
+		if(isAbleToMove(clone)){
+			
+			p=clone;
+			return true;
+		}
+		else 
+			return false;
+		
 	}
-	
-	public boolean ExecLeft( Personnage p){
+		
 
-		p.setDirection(p.getDirectionFromLeft(p.getDirection()));
+	
+	/*
+	public boolean ExecLeft( Character p){
+
+		p.setDirection(p.getDirectionFromLeft(p.getOrientation()));
 		
 			return true;
 			
 		// TODO Auto-generated method stub	
 		
 	}
-	public boolean ExecRight( Personnage p){
+	public boolean ExecRight( Character p){
 
 		p.setDirection(p.getDirectionFromRight(p.getDirection()));
 		
@@ -55,6 +139,7 @@ public class Engine {
 
 	public void setMonde(Map monde) {
 		this.monde = monde;
+		*/
 	}
 
 
